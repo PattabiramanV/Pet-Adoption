51e58970ab73b02a3a42fbfe7b0f7908ec0f910a
madhankumarp1 <madhankumarpdckap@gmail.com>
2024-07-09 12:31:46 +0530

    change the home page

--- src/components/Home/pet card/PetList.jsx
+++ src/components/Home/pet card/PetList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PetCard from './PetCard';
+// import './PetList.css'; // Import your CSS file
 
 const pets = [
   {
@@ -10,7 +11,7 @@ const pets = [
     age: '5 years',
     size: 'Large',
     description: 'Pitter is a friendly, playful, smart male dog. Only adopted to a home...',
-    image: 'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg', // Replace with the actual image path
+    image: 'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg',
     isNew: true
   },
   {
@@ -21,7 +22,7 @@ const pets = [
     age: '2 years',
     size: 'Small',
     description: 'Hero is a playful, smart male cat. You can keep him in an apartment...',
-    image: 'https://www.taiyogroup.in/wp-content/uploads/2022/04/Rabbits.jpg', // Replace with the actual image path
+    image: 'https://www.taiyogroup.in/wp-content/uploads/2022/04/Rabbits.jpg',
   },
   {
     name: 'Magie',
@@ -31,33 +32,28 @@ const pets = [
     age: '27 months',
     size: 'Medium',
     description: 'Magie currently lives with two children age 7 and 13 and has m...',
-    image: 'https://rukminim2.flixcart.com/image/850/1000/j8hnmvk0/poster/a/x/q/large-poster-cute-pet-animal-lover-poster-for-room-cats-dogs-original-imaeyhmksnvkgh48.jpeg?q=90&crop=false', // Replace with the actual image path
+    image: 'https://rukminim2.flixcart.com/image/850/1000/j8hnmvk0/poster/a/x/q/large-poster-cute-pet-animal-lover-poster-for-room-cats-dogs-original-imaeyhmksnvkgh48.jpeg',
   },
-  {
-    name: 'Felix',
-    location: 'New York, USA',
-    gender: 'Female',
-    breed: 'Scottish',
-    age: '9 Months',
-    size: 'Small',
-    description: 'Felix is a cute and playful female cat. She loves waking running a...',
-    image: 'https://friendsofthepound.com/wp-content/uploads/2017/02/foster.jpg', // Replace with the actual image path
-  },
+  // {
+  //   name: 'Felix',
+  //   location: 'New York, USA',
+  //   gender: 'Female',
+  //   breed: 'Scottish',
+  //   age: '9 Months',
+  //   size: 'Small',
+  //   description: 'Felix is a cute and playful female cat. She loves waking running a...',
+  //   image: 'https://friendsofthepound.com/wp-content/uploads/2017/02/foster.jpg',
+  // },
 ];
 
 const PetList = () => {
   return (
     <div className="pet-list">
-      <h1 className='pet-list-name'>Take a Look at Some of <span>Our Pets</span></h1>
+      <h1 className="pet-list-name">Take a Look at Some of <span>Our Pets</span></h1>
       <div className="pet-list-container">
-        <div className="pet-list-container-sub">
-        <PetCard pet={pets[0]} />
-        <PetCard pet={pets[1]} />
-        <PetCard pet={pets[2]} />
-        <PetCard pet={pets[3]} />
-
-        </div>
-
+        {pets.map((pet, index) => (
+          <PetCard key={index} pet={pet} />
+        ))}
       </div>
     </div>
   );
