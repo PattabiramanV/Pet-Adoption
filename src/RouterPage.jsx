b8518fc95399713f6b7b984f7787906e7ac7c36a
madhankumarp1 <madhankumarpdckap@gmail.com>
2024-08-22 11:20:48 +0530

    madhan file add to demo

--- src/RouterPage.jsx
+++ src/RouterPage.jsx
@@ -1,9 +1,18 @@
 // import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PublicRoute from './PublicRoute';
+
+// madhan-----
 import Home from './components/Home';
 import Login from './components/Authentication/Login';
 import Signup from './components/Authentication/RegisterForm';
+import Reset from './components/resetpassword';
+import Map from './components/Location';
+
+// madhan-----
+
+
+
 import PetHostels from './components/PetHostel';
 import Veterinarian from './components/veterinarypage';
 
@@ -20,7 +29,6 @@ import BookHosPage from './components/pethostel/BookHosPage';
 // import GroomingPage  from './components/petgroomingpage';
 import PrivateRoute from './PrivateRoute';
 // import Practice from './components/practice';
-import Reset from './components/resetpassword';
 import DoctorCard from './components/veterinary/carddoctor/doctordata';
 import DoctorMoreInfo from './components/veterinary/carddoctor/moreinfo';
 import Adddoctorform from './components/veterinary/doctoraddform';
@@ -33,7 +41,6 @@ import Doctorpersonaltable from './components/veterinary/docterpersonalpage';
 // import GroomingForm from './components/veterinary/grommingform';
 import Test from './components/text';
 // import Map from './components/Map/map';
-import Map from './components/Location';
 
 
 
@@ -44,19 +51,31 @@ function RouterPage() {
   return (
           <Router>
       <Routes>
+
+
+
+{/* Madhan----------------------- */}
+
         <Route path="/" element={<Home />} />
-        <Route path="/pethostel" element={<PrivateRoute><PetHostels /></PrivateRoute>} />
         <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
         <Route path="/signup" element={<PublicRoute><Signup /></PublicRoute>} />
-        <Route path="/findpet" element={<PrivateRoute><ReunitingLostPets /></PrivateRoute>} />
-        <Route path="/lostingpet" element={<LostingpetAdd />} />
-        <Route path="/LostListPet" element={<LostList />} />
-        <Route path="/mypetlostpost" element={<Mylostpostlist />} />
-        <Route path="/pets" element={<Pets />} />
-        <Route path="/petList" element={<AllPetList />} />
-        <Route path="/bookHos" element={<BookHosPage />} />
-        <Route path="/lostpetlisting" element={<Lostlistallpage />} />
+        <Route path="/reset" element={<PublicRoute><Reset /> </PublicRoute>} />
         <Route path="/map" element={<Map />} />
+
+
+{/* Madhan----------------------- */}
+
+
+        {/* <Route path="/pethostel" element={<PrivateRoute><PetHostels /></PrivateRoute>} /> */}
+
+        {/* <Route path="/findpet" element={<PrivateRoute><ReunitingLostPets /></PrivateRoute>} /> */}
+        {/* <Route path="/lostingpet" element={<LostingpetAdd />} /> */}
+        {/* <Route path="/LostListPet" element={<LostList />} /> */}
+        {/* <Route path="/mypetlostpost" element={<Mylostpostlist />} /> */}
+        {/* <Route path="/pets" element={<Pets />} /> */}
+        {/* <Route path="/petList" element={<AllPetList />} /> */}
+        {/* <Route path="/bookHos" element={<BookHosPage />} /> */}
+        {/* <Route path="/lostpetlisting" element={<Lostlistallpage />} /> */}
 
 
 
@@ -64,27 +83,26 @@ function RouterPage() {
         {/* <Route path="/pet-hostelss" element={<PetHostelss />} /> */}
         {/* <Route path="/Veterinary" element={<Veterinarian />} /> */}
         {/* <Route path="/PetGrooming" element={<GroomingPage />} /> */}
-        <Route path="/reset" element={<PublicRoute><Reset /> </PublicRoute>} />
-        <Route path="/veterinary" element={<Veterinarian />} />
-        <Route path="/petgroomingmainpage" element={<GroomingPage />} />
-        <Route path="/petgrooming" element={<Groomingmainpage />} />
-        <Route path="/doctorcard" element={<DoctorCard />} />
-        <Route path="/doctormoreinfo" element={<DoctorMoreInfo />} />
-        <Route path="/doctoraddform" element={<Adddoctorform />} />
-        <Route path="/groomingusertable" element={< Userpersonaltable/>} />
-        <Route path="/doctorpersonaltable" element={<Doctorpersonaltable/>} />
+        {/* <Route path="/veterinary" element={<Veterinarian />} /> */}
+        {/* <Route path="/petgroomingmainpage" element={<GroomingPage />} /> */}
+        {/* <Route path="/petgrooming" element={<Groomingmainpage />} /> */}
+        {/* <Route path="/doctorcard" element={<DoctorCard />} /> */}
+        {/* <Route path="/doctormoreinfo" element={<DoctorMoreInfo />} /> */}
+        {/* <Route path="/doctoraddform" element={<Adddoctorform />} /> */}
+        {/* <Route path="/groomingusertable" element={< Userpersonaltable/>} /> */}
+        {/* <Route path="/doctorpersonaltable" element={<Doctorpersonaltable/>} /> */}
         {/* <Route path="/pet-hostelss" element={<PetHostelss />} /> */}
 
-        <Route path="/pets" element={<Pets />} />
-        <Route path="/petList" element={<AllPetList />} />
-         <Route path="/petDetails/:id"  element={<Info />} />
-         <Route path="/sale" element={<Sale />} />
+        {/* <Route path="/pets" element={<Pets />} /> */}
+        {/* <Route path="/petList" element={<AllPetList />} /> */}
+         {/* <Route path="/petDetails/:id"  element={<Info />} /> */}
+         {/* <Route path="/sale" element={<Sale />} /> */}
          {/* <Route path="/adopte" element={<Adoption />} /> */}
          {/* <Route path="/salesubmitted" element={<SideBar />} /> */}
-                  <Route path="/test" element={<Test />} />
-        <Route path="/Veterinary" element={<Veterinarian />} />
+                  {/* <Route path="/test" element={<Test />} /> */}
+        {/* <Route path="/Veterinary" element={<Veterinarian />} /> */}
         {/* <Route path="/PetGroomingform" element={<GroomingForm />} /> */}
-        <Route path="/reset" element={<PublicRoute><Reset/></PublicRoute>} />
+        {/* <Route path="/reset" element={<PublicRoute><Reset/></PublicRoute>} /> */}
 
       </Routes>
     </Router>
